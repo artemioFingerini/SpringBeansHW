1a3f8219f837bd178a69c98483ddd9985d074995
artemioFingerini <artemisanx@gmail.com>
2024-09-23 22:36:36 +0300

    commitBeforeReview

--- src/main/java/app/config/AppConfig.java
+++ src/main/java/app/config/AppConfig.java
@@ -1,9 +1,15 @@
 package app.config;
 
+import app.model.AnimalsCage;
+import app.model.Timer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+
+import java.sql.Time;
 
 @Configuration
 @ComponentScan(basePackages = "app")
 public class AppConfig {
+
 }
